279ea85dbf219e6f362af5b80ed07d3c6ea7934c
Harsh Shukla <22f2000147@ds.study.iitm.ac.in>
2025-04-28 12:50:04 +0530

    Delete watch_man_entry_book directory

--- watch_man_entry_book/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = 'super-secret-watchman-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'entry.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- watch_man_entry_book/models.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-from database import db
-
-class Entry(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
